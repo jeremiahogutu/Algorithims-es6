cf309b8567cea0ab48e832933be788888facef8b
Jeremiah Ogutu <jogutu@protonmail.com>
2018-06-23 15:26:01 -0400

    solved steps algorithm

---
+++ steps.js
@@ -0,0 +1,15 @@
+const steps = (n) => {
+    for (let row = 0; row < n; row++) {
+        let stair = '';
+        for (let column = 0; column < n; column++) {
+            if (column <= row) {
+                stair+='#'
+            } else {
+                stair+=''
+            }
+        }
+        console.log(stair)
+    }
+};
+
+steps(5);
